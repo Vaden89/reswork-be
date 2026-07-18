08e4cbae953dab9a6f1f048ebf91b2cf2108a8b3
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-07-18 21:55:56 +0100

    INITIAL COMMIT

---
+++ src/index.ts
@@ -0,0 +1,50 @@
+import "dotenv/config";
+import cors from "cors";
+import express from "express";
+import ApiRouter from "./routers/router.js";
+import { createError } from "./utils/app-error.js";
+import { errorHandler } from "./middleware/error.middleware.js";
+import os from "os";
+
+import type { Response } from "express";
+
+const PORT = process.env.PORT ?? 3001;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: [
+      process.env.CORS_ORIGIN || "http://localhost:3000",
+      "http://localhost:3000",
+    ],
+    methods: ["GET", "POST", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
+
+app.get("/health", (req, res: Response) => {
+  const uptime = os.uptime();
+  const memoryUsage = os.totalmem() / os.freemem();
+  res.status(200).json({
+    data: {
+      uptime,
+      memoryUsage: memoryUsage.toFixed(2),
+    },
+    message: "alive",
+  });
+});
+
+app.use("/api", ApiRouter);
+
+app.use((_req, _res, next) => {
+  next(createError("Not found", 404));
+});
+
+app.use(errorHandler);
+
+app.listen(PORT, () => {
+  console.log(`Application is running on port: ${PORT}`);
+});
