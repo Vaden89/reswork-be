a3d02594efe77beadb089b6bb8f7c3d9dbc7271a
Isaac Shosanya <isaacshosanya89@gmail.com>
2026-07-20 13:32:55 +0100

    fix #4

--- src/index.ts
+++ src/index.ts
@@ -15,12 +15,6 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(
-  cors({
-    origin: process.env.CORS_ORIGIN,
-    allowedHeaders: ["Content-Type", "Authorization"],
-  }),
-);
 
 app.get("/health", (req, res: Response) => {
   const uptime = os.uptime();
